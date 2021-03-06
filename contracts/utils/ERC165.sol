156da60593e309c0e279660617b0e7b11fa4562d
Chelbukhov <chelbukhov@gmail.com>
2021-03-06 14:15:01 +0300

    some changes

--- contracts/utils/ERC165.sol
+++ contracts/utils/ERC165.sol
@@ -7,7 +7,9 @@ abstract contract ERC165 {
    * @param _interfaceID The interface identifier, as specified in ERC-165
    * @return `true` if the contract implements `_interfaceID`
    */
+/*
   function supportsInterface(bytes4 _interfaceID) virtual public pure returns (bool) {
     return _interfaceID == this.supportsInterface.selector;
   }
+*/
 }
